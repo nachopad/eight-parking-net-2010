d0d782bf2999df691313c5e4f66c2ab98ca6264c
Carlos Fernando Mamani <carlosfernando21334@gmail.com>
2023-09-16 16:54:56 -0300

    Se crea el diseño de la ventana 'Vehiculos en Playa'

---
+++ LPOOII_GRUPO08/Vistas/VehiculosPlaya.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Vistas
+{
+    /// <summary>
+    /// Interaction logic for VehiculosPlaya.xaml
+    /// </summary>
+
+    public partial class VehiculosPlaya : Window
+    {
+        private const string disponible = "#FF008000";
+        private const string deshabilitado = "#FF808080";
+        private const string  ocupado ="#FFFF0000";
+
+        public VehiculosPlaya()
+        {
+            InitializeComponent();
+
+            Loaded += VehiculosPlaya_Loaded;
+         
+
+        }
+       private void VehiculosPlaya_Loaded(object sender, RoutedEventArgs e)
+    {
+        e7.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString(ocupado));
+    }
+
+        
+
+        private void Button_click(object sender, RoutedEventArgs e)
+        {
+            Button clickedButton = (Button)sender;
+            SolidColorBrush backgroundBrush = clickedButton.Background as SolidColorBrush;
+
+            switch (backgroundBrush.Color.ToString())
+            {
+                case disponible:
+                    MessageBox.Show("Sector Disponible, registrar entrada");
+                    break;
+                case deshabilitado:
+                    MessageBox.Show("Sector deshabilitado");
+                    break;
+                case ocupado:
+                    MessageBox.Show("Sector Ocupado, registrar salida");
+                    break;
+            }
+        }
+
+        private void salir_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+       
+    }
+}
