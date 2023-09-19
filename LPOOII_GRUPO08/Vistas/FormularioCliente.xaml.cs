83b22320963cda2d0b87b494dd490ef4777c1bcc
Carlos Sanchez <argcarlos688@gmail.com>
2023-09-18 21:16:32 -0300

    Agregando el formulario para el registro de clientes y vehiculos

---
+++ LPOOII_GRUPO08/Vistas/FormularioCliente.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using ClasesBase;
+
+namespace Vistas
+{
+    /// <summary>
+    /// Interaction logic for FormularioCliente.xaml
+    /// </summary>
+    public partial class FormularioCliente : Window
+    {
+        public FormularioCliente()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRegistrar_Click(object sender, RoutedEventArgs e)
+        {
+            Cliente cliente = new Cliente();
+
+            if (txtApellido.Text.Equals("") || txtDNI.Text.Equals("") || txtNombre.Text.Equals("") || txtTelefono.Text.Equals(""))
+            {
+                MessageBox.Show("Debe ingresar todos los datos solicitados");
+            }
+            else
+            {
+                cliente.ClienteDNI = txtDNI.Text;
+                cliente.Nombre = txtNombre.Text;
+                cliente.Apellido = txtApellido.Text;
+                cliente.Telefono = txtTelefono.Text;
+                MessageBox.Show("Nombre:  " + cliente.Nombre + " - Apellido: " + cliente.Apellido + " - DNI: " + cliente.ClienteDNI + " - Telefono: " + cliente.Telefono);
+            }
+        }
+    }
+}
---
+++ LPOOII_GRUPO08/Vistas/FormularioVehiculo.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using ClasesBase;
+
+namespace Vistas
+{
+    /// <summary>
+    /// Interaction logic for FormularioVehiculo.xaml
+    /// </summary>
+    public partial class FormularioVehiculo : Window
+    {
+        public FormularioVehiculo()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRegistrar_Click(object sender, RoutedEventArgs e)
+        {
+            TipoVehiculo vehiculo = new TipoVehiculo();
+            if (txtDescripcion.Text.Equals("") || txtTarifa.Text.Equals(""))
+            {
+                MessageBox.Show("Debe ingresar todos los valores");
+            }
+            else
+            {
+                vehiculo.Descripcion = txtDescripcion.Text;
+                vehiculo.Tarifa = decimal.Parse(txtTarifa.Text);
+                MessageBox.Show("Descripcion del vehiculo: " + vehiculo.Descripcion + " - Tarifa del vehiculo: " + vehiculo.Tarifa);
+            }
+        }
+    }
+}
