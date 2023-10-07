c466ef8eb14cb32cabae786a9e547ce0d5b8acff
Carlos Sanchez <argcarlos688@gmail.com>
2023-10-07 14:38:39 -0300

    Agregando UserControl para el login

--- LPOOII_GRUPO08/Vistas/Login.xaml.cs
+++ LPOOII_GRUPO08/Vistas/Login.xaml.cs
@@ -24,33 +24,6 @@ namespace Vistas
             InitializeComponent();
         }
 
-        private void buttonAceptar_Click(object sender, RoutedEventArgs e)
-        {
-            Usuario administrador = new Usuario("administrador", "123", "admin", "admin", "1");
-            Usuario operador = new Usuario("operador", "123", "oper", "oper", "2");
-            List<Usuario> listUsers = new List<Usuario>();
-            listUsers.Add(administrador);
-            listUsers.Add(operador);
-
-            Usuario buscarUsu = listUsers.Find(p => p.UserName.Equals(textBoxUser.Text));
-
-            if (buscarUsu != null && buscarUsu.Password.Equals(textBoxPsw.Password))
-            {
-                MessageBox.Show("Bienvenido al Sistema " + buscarUsu.Nombre, "Acceso concedido", MessageBoxButton.OK);
-                MenuPrincipal menuPrincipal = new MenuPrincipal(buscarUsu.Rol);
-                menuPrincipal.Show();
-                this.Close();
-            }
-            else
-            {
-                MessageBox.Show("El usuario y/o contraseña no son válidos. Vuelve a intentarlo.", "Error al iniciar sesión", MessageBoxButton.OK, MessageBoxImage.Hand);
-            }
-
-        }
-
-        private void button1_Click(object sender, RoutedEventArgs e)
-        {
-            Application.Current.Shutdown();
-        }
+        
     }
 }
