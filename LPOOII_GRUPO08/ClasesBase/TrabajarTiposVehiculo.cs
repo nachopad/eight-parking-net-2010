d11ca22790aa6fb20d2a254e19571a7778cc2c04
Ignacio Agustin Padilla <nacho20002008@gmail.com>
2023-10-07 19:35:28 -0300

    se crea Base de Datos "playa" y se implementa grilla para la clase TipoVehiculo, se realizan puntos 1 y 2 - TP3.

---
+++ LPOOII_GRUPO08/ClasesBase/TrabajarTiposVehiculo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ClasesBase
+{
+    public class TrabajarTiposVehiculo
+    {
+
+        public DataTable TraerTiposVehiculo()
+        {
+            DataTable dt = new DataTable();
+            using (SqlConnection connection = new SqlConnection("Data Source=.\\SQLEXPRESS;AttachDbFilename=C:\\Users\\lenovo\\Documents\\LPOOII_GRUPO08\\LPOOII_GRUPO08\\playa.mdf;Integrated Security=True;Connect Timeout=30;User Instance=True"))
+            {
+                connection.Open();
+                string query = "SELECT tv_codigo, descripcion, tarifa FROM TipoVehiculo";
+                SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+                adapter.Fill(dt);
+            }
+            return dt;
+        }
+
+    }
+}
