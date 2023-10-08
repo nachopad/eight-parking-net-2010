817f01552f2ab492e0163570c426f67682e7529c
MaxiCarrillo <maxi11carrillo@gmail.com>
2023-10-08 18:32:57 -0300

    Creacion de Tiempoxml y correcion en el menu principal

--- LPOOII_GRUPO08/ClasesBase/TrabajarTiposVehiculo.cs
+++ LPOOII_GRUPO08/ClasesBase/TrabajarTiposVehiculo.cs
@@ -13,7 +13,7 @@ namespace ClasesBase
         public DataTable TraerTiposVehiculo()
         {
             DataTable dt = new DataTable();
-            using (SqlConnection connection = new SqlConnection("Data Source=.\\SQLEXPRESS;AttachDbFilename=C:\\Users\\lenovo\\Documents\\LPOOII_GRUPO08\\LPOOII_GRUPO08\\playa.mdf;Integrated Security=True;Connect Timeout=30;User Instance=True"))
+            using (SqlConnection connection = new SqlConnection("Data Source=.\\SQLEXPRESS;AttachDbFilename=C:\\Users\\maxi1\\OneDrive\\Documentos\\Programacion LPOO II\\LPOOII_GRUPO08\\LPOOII_GRUPO08\\playa.mdf;Integrated Security=True;Connect Timeout=30;User Instance=True"))
             {
                 connection.Open();
                 string query = "SELECT tv_codigo, descripcion, tarifa FROM TipoVehiculo";
