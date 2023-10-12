3ef2bd8bbbe8d8cd670bcc2affbbd9fcb4933db2
Carlos Sanchez <argcarlos688@gmail.com>
2023-10-12 11:35:33 -0300

    Agregando la ventana para validar datos del cliente

--- LPOOII_GRUPO08/ClasesBase/Cliente.cs
+++ LPOOII_GRUPO08/ClasesBase/Cliente.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.ComponentModel;
+using System.Text.RegularExpressions;
 
 namespace ClasesBase
 {
-    public class Cliente
+    public class Cliente:IDataErrorInfo
     {
         private string clienteDNI;
         private string apellido;
@@ -35,6 +37,42 @@ namespace ClasesBase
             get { return telefono; }
             set { telefono = value; }
         }
+
+        public string Error
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public string this[string columnName]
+        {
+            get
+            {
+                string result = null;
+                if (columnName == "ClienteDNI")
+                {
+                    if (String.IsNullOrEmpty(ClienteDNI) || ClienteDNI.Length < 8 || ClienteDNI.Length > 8)
+                        result = "Debe ingresar un DNI con 8 digitos numericos";
+
+                }
+                else if (columnName == "Nombre")
+                {
+                    if (String.IsNullOrEmpty(Nombre) || Nombre.Length < 3)
+                        result = "Debe ingresar el nombre minimo 3 caracteres";
+                }
+                else if (columnName == "Apellido")
+                {
+                    if (String.IsNullOrEmpty(Apellido) || Apellido.Length < 3)
+                        result = "Debe ingresar un apellido minimo de 3 caracteres";
+                }
+                else if (columnName == "Telefono")
+                {
+                    if (String.IsNullOrEmpty(Telefono))
+                        result = "Debe ingresar un numero de telefono";
+                }
+                return result;
+            }
+        }
+
 
     }
 }
