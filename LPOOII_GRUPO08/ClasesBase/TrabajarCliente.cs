256391d33d905fe99f386fd3d7d9bb399fd55b8a
Carlos Fernando Mamani <carlosfernando21334@gmail.com>
2023-10-27 23:54:06 -0300

    se agrega punto 4e

---
+++ LPOOII_GRUPO08/ClasesBase/TrabajarCliente.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ClasesBase
+{
+    public class TrabajarCliente
+    {
+        public Cliente ObtenerClientePorDni(string dni)
+        {
+            Cliente cliente = new Cliente();
+            string connectionString = "Data Source=.\\SQLEXPRESS;AttachDbFilename=D:\\NET\\LPOOII_GRUPO08\\LPOOII_GRUPO08\\playa.mdf;Integrated Security=True;Connect Timeout=30;User Instance=True";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                using (SqlCommand command = new SqlCommand("SELECT * FROM Cliente WHERE cliente_dni = @Dni", connection))
+                {
+                    command.Parameters.AddWithValue("@Dni", dni);
+
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    if (reader.Read())
+                    {
+
+                        cliente.ClienteDNI = reader["cliente_dni"].ToString();
+                        cliente.Nombre = reader["nombre"].ToString();
+                        cliente.Apellido = reader["apellido"].ToString();
+                        cliente.Telefono = reader["telefono"].ToString();
+                    }
+                }
+            }
+
+            return cliente;
+        }
+    }
+}
---
+++ LPOOII_GRUPO08/Vistas/ModificarCliente.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using ClasesBase; 
+namespace Vistas
+{
+    /// <summary>
+    /// Interaction logic for ModificarCliente.xaml
+    /// </summary>
+    public partial class ModificarCliente : Window
+    {
+        public ModificarCliente()
+        {
+            InitializeComponent();
+            Cliente cliente = new Cliente();
+            this.DataContext = cliente;
+        }
+
+        private void btnSalir_Click(object sender, RoutedEventArgs e)
+        {
+            MenuPrincipal menuPrincipal = new MenuPrincipal("2");
+            menuPrincipal.Show();
+            this.Close();
+        }
+
+        private void txtDNI_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            // Crear una instancia de TrabajarClientes
+            TrabajarCliente trabajador = new TrabajarCliente();
+
+            // Obtener el cliente con el DNI ingresado
+            Cliente cliente = trabajador.ObtenerClientePorDni(txtDNI.Text);
+
+            // Actualizar los TextBox con los datos del cliente
+            txtApellido.Text = cliente.Apellido;
+            txtNombre.Text = cliente.Nombre;
+            txtTelefono.Text = cliente.Telefono;
+        }
+
+
+    }
+}
