f1f7b734ead4144d5e32383976a756708f752baa
Lilia Said <yazminsaid@gmail.com>
2023-11-16 22:23:04 -0300

    se sube prototipo de vista Ticket tp5 2.b FixedDocs.xaml

---
+++ LPOOII_GRUPO08/Vistas/FixedDocs.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Vistas
+{
+    /// <summary>
+    /// Interaction logic for FixedDocs.xaml
+    /// </summary>
+    public partial class FixedDocs : Window
+    {
+        public FixedDocs()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            txbNombrePlayaEstacionamiento.Text = "PLAYA DE ESTACIONAMIENTO GRUPO 08";
+            txbDireccion.Text = "DOMICILIO: " + "Alberdi 777  S.S De Jujuy";
+            txbCuit.Text = "CUIT: " + "30-88888888-9";
+            txbIibb.Text = "IIBB: " + "999";
+            txbNumeroTicket.Text = "TICKET #" + "1111";
+            txbPatente.Text = "PATENTE: " + "AB235A";
+            txbTipoVehiculo.Text = "TIPO VEHICULO: " + "Auto";
+            txbCliente.Text = "CLIENTE: " + "Juan Perez";
+            txbIngreso.Text = "HORA DE INGRESO: " + "10:40";
+            txbTarifa.Text = "TARIFA: " + "500";
+            txbUsuario.Text = "USUARIO: " + "Operador";
+        }
+    }
+}
